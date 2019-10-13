c5ac27a65c6297219c72830b064e48a1136c0c68
PrettyBoyHelios <helios@polispay.org>
2019-10-13 11:28:02 -0500

    Initial Navigation

--- src/app/pages/food/food.page.spec.ts
+++ src/app/pages/food/food.page.spec.ts
@@ -1,21 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TabsPage } from './tabs.page';
+import { FoodPage } from './food.page';
 
-describe('TabsPage', () => {
-  let component: TabsPage;
-  let fixture: ComponentFixture<TabsPage>;
+describe('FoodPage', () => {
+  let component: FoodPage;
+  let fixture: ComponentFixture<FoodPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [TabsPage],
+      declarations: [ FoodPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    }).compileComponents();
+    })
+    .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TabsPage);
+    fixture = TestBed.createComponent(FoodPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
