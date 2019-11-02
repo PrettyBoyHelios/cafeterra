a6d31f71c9038592546d980368c0e95c1ec43534
Erick Gutiérrez <erickgtzh@gmail.com>
2019-11-01 20:38:14 -0600

    Log-In almost ready

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
+
 export class ProfilePage implements OnInit {
 
   email: string;
   password: string;
-  
-  constructor() { }
+
+  constructor(private authService: AuthService, public router: Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmitLogin()
+  {
+    this.authService.login(this.email, this.password).then( res =>{
+      this.router.navigate(['/home']);
+    }).catch(err => alert('los datos son incorrectos o no existe el usuario'))
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,16 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyCjT9DMpZmI6xPmsm6qjT3NKKNJ0FjHJfA",
+  authDomain: "cafeterraup.firebaseapp.com",
+  databaseURL: "https://cafeterraup.firebaseio.com",
+  projectId: "cafeterraup",
+  storageBucket: "cafeterraup.appspot.com",
+  messagingSenderId: "381020204681",
+  appId: "1:381020204681:web:cfef7a11284771f179e058",
+  measurementId: "G-BKZEDSGCMG"
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
