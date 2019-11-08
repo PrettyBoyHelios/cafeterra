b9e6bba3eef38ed36ae3b191e3cc979d527d2b67
Efrén Gonzalez <0192704@up.edu.mx>
2019-11-07 21:10:28 -0600

    Add add product

--- src/app/pages/food/food.page.ts
+++ src/app/pages/food/food.page.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from '../../services/product.service';
 import {Product} from '../../models/product/product';
+import {Store} from '../../models/store';
 import {Router} from '@angular/router';
 import {ShoppingCartService} from '../../services/shopping-cart/shopping-cart.service';
+import {storage} from 'firebase';
 
 
 @Component({
@@ -15,6 +17,24 @@ export class FoodPage implements OnInit {
   private resultProducts: Product[];
   private searchTerm: string;
   private isSearching = false;
+  private name: string;
+  private price: number;
+  private amount: number;
+  private description: string;
+  private store: Store = {
+    storeName: undefined,
+    storeId: undefined
+  }
+  private newProduct: Product = {
+    name: undefined,
+    available: undefined,
+    description: undefined,
+    image: undefined,
+    price: undefined,
+    rating: undefined,
+    timesBought: undefined,
+    store: this.store
+  };
   constructor(
       private prodService: ProductService,
       private shopService: ShoppingCartService,
@@ -30,6 +50,20 @@ export class FoodPage implements OnInit {
   async addProductToShoppingCart(item: Product) {
     await this.shopService.addProduct(item);
     this.searchTerm = '';
+  }
+
+  async addProductToDB() {
+    this.newProduct.name = this.name;
+    this.newProduct.description = this.description;
+    this.newProduct.price = this.price;
+    this.newProduct.available = true;
+    this.newProduct.timesBought = 0;
+    this.newProduct.rating = 0;
+    this.newProduct.image = '';
+    this.newProduct.store.storeName = '';
+    this.newProduct.store.storeId = '1';
+    console.log(this.newProduct);
+    await this.prodService.addProduct(this.newProduct);
   }
 
   public onSearchTerm() {
