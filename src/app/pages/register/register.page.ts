ea27696e599cb859b573d88223ecfa481aa40ce3
PrettyBoyHelios <helios@polispay.org>
2019-11-08 18:00:04 -0600

    Update register.page.ts

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from "../../services/auth.service";
-import { Router } from "@angular/router";
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -18,10 +18,9 @@ export class RegisterPage implements OnInit {
   ngOnInit() {
   }
 
-  OnSubmitRegister(){
+  OnSubmitRegister() {
     this.auth.register(this.email, this.password, this.name).then( auth => {
-      this
       this.router.navigate(['/tabs/food']);
-    }).catch(err => console.log(err))
+    }).catch(err => console.log(err));
   }
 }
